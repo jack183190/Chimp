2fc50a0d0c72cd1f5a9961a6db9006e681e60271
jack183190 <119117553+jack183190@users.noreply.github.com>
2024-10-18 15:44:14 +0100

    removed impl for ECS.h if there is no ecs library

--- Chimp/src/api/ecs/ECS.h
+++ Chimp/src/api/ecs/ECS.h
@@ -32,31 +32,5 @@ namespace Chimp {
 	private:
 		flecs::world m_World;
 	};
-#else
-	typedef int EntityId;
-	class ECS {
-	public:
-		ECS() = default;
-
-		template <typename... Components>
-		using ComponentsRefFunc = std::function<void(Components&...)>;
-
-		// Create an entity with a list of components
-		// Components - Types of components the entity should have
-		// func - This function will be called once the entity is created, it should set data of components to their default values
-		template <typename... Components>
-		EntityId CreateEntity(const ComponentsRefFunc<Components...>& func) {
-			assert(false);
-			return 0;
-		}
-
-		// Get all entities with all of a set of components
-		// Components - Types of components the entity should have
-		// func - This function will be called once for every entity with all the specified components
-		template <typename... Components>
-		void GetEntitiesWithComponents(const ComponentsRefFunc<Components...>& func) {
-			assert(false);
-		}
-	};
 #endif
 }
